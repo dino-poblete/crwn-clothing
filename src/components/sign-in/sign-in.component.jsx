efe832f51d50298300dbb98f24dfe6cf59bc1dd5
Dino Poblete <dino.poblete@gmail.com>
2021-07-23 23:30:24 +0800

    Fix form input and button

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -59,7 +59,11 @@ class SignIn extends Component {
             <CustomButton type="submit" onClick={this.handleSubmit}>
               Sign In
             </CustomButton>
-            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+            <CustomButton
+              type="button"
+              onClick={signInWithGoogle}
+              isGoogleSignIn
+            >
               Sign in with Google
             </CustomButton>
           </div>
