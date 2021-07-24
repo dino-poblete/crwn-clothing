907b12887b8b5d1a3a36b37ebf04eb41806cb776
Dino Poblete <dino.poblete@gmail.com>
2021-07-24 22:23:51 +0800

    Add UserActionTypes

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./collection-item.styles.scss";
 
 const CollectionItem = ({ id, name, price, imageUrl }) => (
-  <div class="collection-item">
+  <div className="collection-item">
     <div
       className="image"
       style={{
